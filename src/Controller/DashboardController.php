4f61ac0a60f3a2a6671b5b427c2ee9e25d777c6f
elyachtdev <elliotnft@gmail.com>
2022-06-07 18:02:34 +0200

    Auth setup

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api", name="api_")
+ */
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DashboardController.php',
+        ]);
+    }
+}
