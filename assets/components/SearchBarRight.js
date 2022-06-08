de264b80b412e513040ea5059f07d3bfa8710c45
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-08 14:21:44 +0200

    corrigé l'id de la searchbar de droite

--- assets/components/SearchBarRight.js
+++ assets/components/SearchBarRight.js
@@ -1,6 +1,6 @@
 function SearchBarRight({searchValue, updateValue}) {
 
-    return <input id="search-bar-left" type="text" onChange={
+    return <input id="search-bar-right" type="text" onChange={
         function(e){
             updateValue(e.target.value)
         }
