de36af742027337589297b4f46cc9bf463574b20
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-08 15:14:20 +0200

    Ajout page Compare.js et compo AffichageData.js

---
+++ assets/pages/compare/Compare.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+import AffichageData from '../../components/AffichageData'
+import PanneauSelection from '../../components/PanneauSelection'
+
+function Compare(){
+    return <div id="comp-container">
+        <PanneauSelection />
+        <AffichageData />
+    </div>
+}
+
+export default Compare
