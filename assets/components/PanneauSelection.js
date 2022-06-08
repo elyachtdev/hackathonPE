c42f761975f253822ca3ad10ad5babb42cb21c15
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-08 14:06:30 +0200

    ajout d'un premier comp input et d'un useState pour récup le string

--- assets/components/PanneauSelection.js
+++ assets/components/PanneauSelection.js
@@ -1,7 +1,15 @@
+import {useState} from 'react'
+import SearchBarLeft from './SearchBarLeft'
+import SearchBarRight from './SearchBarRight'
+
 function PanneauSelection() {
+    const [searchValueLeft, updateSearchValueLeft] = useState(" ");
+    const [searchValueRight, updateSearchValueRight] = useState(" ");
     return <div id="panneau-selection">
-        <SearchBar />
+        <SearchBarLeft searchValue={searchValueLeft} updateValue={updateSearchValueLeft} />
         <p>Comparer avec un autre produit : </p>
-        <SearchBar />
+        <SearchBarRight searchValue={searchValueRight} updateValue={updateSearchValueRight} />
     </div>
 }
+
+export default PanneauSelection
