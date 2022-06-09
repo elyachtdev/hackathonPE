84219c37dd3d11963dc2de66fd456481f1c8ea6b
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-09 13:42:34 +0200

    on avance LA PAGE S AFFICHE avec les barres de recherches mais tjrs pas la liste

--- assets/components/Positionnement.js
+++ assets/components/Positionnement.js
@@ -1,17 +1,14 @@
 import React from "react";
-//import PosLI from "./PosLI";
+import PosLI from "./PosLI";
 
 function Positionnement(objArray){
     const magasin = objArray;
 
     return <ul id="liste-pos">
-        {magasin.map(function (mag){
-            return <li key={mag._id}>
-            <p>{mag.product_name}</p>
-            <p>{mag.stores}</p>
-        </li> /*<PosLI id={mag._id} name={mag.product_name} store={mag.stores} />*/
+        {()=>{ magasin.map(function (mag){
+            return <PosLI id={mag._id} name={mag.product_name} store={mag.stores} />
         }
-    )}
+    )}}
     </ul>
 }
 
