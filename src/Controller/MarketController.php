c2be7c36b62783cc8a8133dd32064d87113e10ae
elyachtdev <elliotnft@gmail.com>
2022-06-09 11:37:01 +0200

    Update MarketController.php

--- src/Controller/MarketController.php
+++ src/Controller/MarketController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\RankedPage;
 use App\Repository\RankedPageRepository;
+use App\Repository\KeywordRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,14 @@ class MarketController extends AbstractController
     {
         // $min_rank = 50000;
         $markets = $repo->findSearchRanking();
+
+        return $this->json($markets);
+    }
+    #[Route('/market2', name: 'app_market')]
+    public function test(KeywordRepository $repo): Response
+    {
+        // $min_rank = 50000;
+        $markets = $repo->findSearchCount();
 
         return $this->json($markets);
     }
