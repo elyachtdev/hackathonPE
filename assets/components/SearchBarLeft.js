7e060ff9404b9b349d45d9d2780dd23d714b4ebb
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-09 11:36:00 +0200

    tentatives d'affichage ratées en commentaire

--- assets/components/SearchBarLeft.js
+++ assets/components/SearchBarLeft.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 //compo barre de recherche de gauche, prend le useState en props
-function SearchBarLeft({searchValue, updateValue}) {
+function SearchBarLeft({searchValue, updateValue, /*display*/}) {
+    //tentative d'affichage du compo onClick button ds parent
+    //let displayStyle = display ? "display:block" : "display:none";
 
-    return <input id="search-bar-left" type="text" size="18" maxLength="16" onChange={
+    return <input /*style={displayStyle}*/ id="search-bar-left" type="text" size="18" maxLength="16" onChange={
         function(e){
             updateValue(e.target.value) //envoie l'input en props
         }
--- assets/components/SearchBarRight.js
+++ assets/components/SearchBarRight.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 //compo barre de recherche de droite, prend le useState en props
-function SearchBarRight({searchValue, updateValue}) {
+function SearchBarRight({searchValue, updateValue, /*display*/}) {
+    //tentative d'affichage du compo onClick button ds parent
+    //let displayStyle = display ? "display:block" : "display:none";
 
-    return <input id="search-bar-right" type="text" size="18" maxLength="16" onChange={
+    return <input /*style={displayStyle}*/ id="search-bar-right" type="text" size="18" maxLength="16" onChange={
         function(e){
             updateValue(e.target.value) //envoie l'input en props
         }
