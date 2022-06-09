48135245fc60d9b99c86e76fe76bf8cf50d630db
Sepulturagnarok <mindstrife@hotmail.com>
2022-06-09 12:11:20 +0200

    sauvegardé le compo affichageData

--- assets/components/AffichageData.js
+++ assets/components/AffichageData.js
@@ -2,10 +2,11 @@ import React from "react"
 import Positionnement from "./Positionnement"
 import { useState } from "react"
 
+
+
 //le composant qui affiche les résultats de notre comparaison
-//il est un peu vide pour l'instant :(
 function AffichageData(){
-    //on passe la liste d'objets dans ce state
+    //on passe la liste d'objets dans ce state : 
     const [listeMagasins, updateListeMagasins] = useState([]);
 
     return <div>
