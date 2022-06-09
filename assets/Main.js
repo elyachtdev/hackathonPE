8a44c9fb1d3e9413af02ba3f7796ae378b8aa85e
elyachtdev <elliotnft@gmail.com>
2022-06-09 11:37:21 +0200

    Update Main.js

--- assets/Main.js
+++ assets/Main.js
@@ -7,12 +7,13 @@ import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import SignUp from "./pages/auth/Signup";
 import Compare from "./pages/compare/Compare";
-import ProjectList from "./pages/project/ProjectList";
-import ProjectCreate from "./pages/project/ProjectCreate";
-import ProjectEdit from "./pages/project/ProjectEdit";
-import ProjectShow from "./pages/project/ProjectShow";
+// import ProjectList from "./pages/project/ProjectList";
+// import ProjectCreate from "./pages/project/ProjectCreate";
+// import ProjectEdit from "./pages/project/ProjectEdit";
+// import ProjectShow from "./pages/project/ProjectShow";
 import RechercheApiBeauty from "./pages/rechercheApi/RechercheApiBeauty";
 import FoodSearch from "./pages/food/foodSearch.js";
+import Popularity from "./pages/popularity/Popularity";
 
 
 function Main() {
@@ -24,11 +25,12 @@ function Main() {
           <Route path="/RechercheApiBeauty" element={<RechercheApiBeauty/>}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
           <Route path="/compare" element={<Compare />} />
-          <Route exact path="/projects/" element={<ProjectList />} />
+          {/* <Route exact path="/projects/" element={<ProjectList />} /> */}
           <Route exact path="/food/" element={<FoodSearch />} />
-          <Route path="/projects/create" element={<ProjectCreate />} />
+          {/* <Route path="/projects/create" element={<ProjectCreate />} />
           <Route path="/projects/edit/:id" element={<ProjectEdit />} />
-          <Route path="/projects/show/:id" element={<ProjectShow />} />
+          <Route path="/projects/show/:id" element={<ProjectShow />} /> */}
+          <Route path="/popularity" element={<Popularity />} />
         </Routes>
       </Layout>
     </Router>
